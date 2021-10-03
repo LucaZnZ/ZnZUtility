7d490e18318986d54b2e65f63d631e5ebb0216d4
LucaZ <Luca@znzproductions.net>
2021-10-03 12:05:36 +0200

    Added Singleton Base class

---
+++ Assets/Scripts/SingletonBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public abstract class SingletonBase<T> : MonoBehaviour where T : class
+{
+    protected static T instance;
+
+    public virtual void Awake()
+    {
+        if (instance == null)
+            instance = this as T;
+        else
+        {
+            Debug.LogWarning("Destroyed Singleton Object: " + name);
+            Destroy(this);
+        }
+    }
+}
