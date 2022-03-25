2dbc859de238d98686dd1c4350d59758699729c8
LucaZ <Luca@znzproductions.net>
2022-03-25 19:33:40 +0100

    Added Logger to easy access unity logger from other assemblies... Not ideal, i admit, but working

--- Assets/Scripts/FloatUtil.cs
+++ Assets/Scripts/FloatUtil.cs
@@ -1,26 +1,26 @@
 ﻿namespace ZnZUtil
 {
-	public static class FloatUtil
-	{
-		public static float SmallParse(string parse)
-		{
-			float one = float.Parse(parse.Replace('.', ','));
-			float two = float.Parse(parse.Replace(',', '.'));
-			return one > two ? two : one;
-		}
+    public static class FloatUtil
+    {
+        public static float SmallParse(string parse)
+        {
+            float one = float.Parse(parse.Replace('.', ','));
+            float two = float.Parse(parse.Replace(',', '.'));
+            return one > two ? two : one;
+        }
 
-		public static bool TrySmallParse(string parse, out float result)
-		{
-			try
-			{
-				result = SmallParse(parse);
-				return true;
-			}
-			catch
-			{
-				result = 0;
-				return false;
-			}
-		}
-	}
+        public static bool TrySmallParse(string parse, out float result)
+        {
+            try
+            {
+                result = SmallParse(parse);
+                return true;
+            }
+            catch
+            {
+                result = 0;
+                return false;
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace ZnZUtil
+{
+    public static class Logger
+    {
+        public static void Log(object message)
+        {
+            Debug.Log(message);
+        }
+
+        public static void LogException(object message, Exception exception)
+        {
+            Debug.LogWarning(message + "Exception: \n" + exception.Message);
+        }
+
+        public static void LogWarning(object message)
+        {
+            Debug.LogWarning(message);
+        }
+    }
+}
