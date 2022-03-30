f5f1f0d0b4f2cbb43cedce1629af7b62fd3af8dd
LucaZ <Luca@znzproductions.net>
2022-03-30 19:44:43 +0200

    Basic Action System with: Actor.cs Command Pattern invoker to perform actions ITransitionGraph.cs Factory to create Transitions from current constraints to next IConstraint.cs Constraints that drive what actions are currently possible and can be combined. IInteraction.cs Are represent interactions able to play also provides methods to cancel them ITarget.cs represents targets the player and npcs can interact with, maybe it will offer an interface to show available interactions to the player/a character Also provided implementations of the System with unity as well as an example scene where the system is used to walk somewhere and transition from that position to walk towards, rotate and sit on(in) a box.

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ZnZUtil
+{
+    public abstract class Singleton<T> where T : class
+    {
+        // protected static T instance;
+        private static readonly Lazy<T> instance = new Lazy<T>();
+        public static T GetInstance() => instance.Value;
+    }
+}
